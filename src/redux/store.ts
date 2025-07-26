3067048618ddc725bed6934aafe53fdbb77cc235
Radiat09 <rzsianboyridoy10@gmail.com>
2025-07-26 13:30:00 +0600

    refactor(redux): remove dead cartSlice - cart is managed by CartContext

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
 import filterReducer from "./features/filter/filterSlice";
-import cartReducer from "./features/cart/cartSlice";
 
 export const makeStore = () =>
   configureStore({
     reducer: {
       user: userReducer,
       filter: filterReducer,
-      cart: cartReducer,
     },
   });
 
